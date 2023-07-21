a956e3005a1386049b439c6b13d52d6188eac832
veekshith21 <83058645+veekshith21@users.noreply.github.com>
2023-07-21 11:48:08 +0530

    intial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const quoteText = document.querySelector("#text");
+const quoteBtn = document.querySelector("#new-quote");
+const authorName = document.querySelector("#author");
+const twitterBtn = document.querySelector("#tweet-quote");
+const copyBtn = document.querySelector('#copy');
+
+function randomQuote() {
+  quoteBtn.classList.add("loading");
+  quoteBtn.innerText = "Loading Quote...";
+  fetch("http://api.quotable.io/random")
+    .then((response) => response.json())
+    .then((result) => {
+      quoteText.innerText = result.content;
+      authorName.innerText = result.author;
+      quoteBtn.classList.remove("loading");
+      quoteBtn.innerText = "New Quote";
+
+    });
+}
+
+twitterBtn.addEventListener("click", () => {
+  let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+  window.open(tweetUrl, "_blank");
+});
+
+copyBtn.addEventListener("click", ()=>{
+  copyBtn.classList.add("copied");
+  copyBtn.innerText = "C";
+  navigator.clipboard.writeText(quoteText.innerText);
+});
+
+quoteBtn.addEventListener("click", randomQuote);
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
